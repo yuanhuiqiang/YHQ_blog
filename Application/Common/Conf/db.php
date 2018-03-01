ee072abbf514a6dc1b9d4b3dd5de7028247d9f6d
yuan <99@qq.com>
2018-03-01 23:16:46 +0800

    d

--- Application/Common/Conf/db.php
+++ Application/Common/Conf/db.php
@@ -3,11 +3,11 @@
 return array(
 	'DB_TYPE' => 'mysql', //数据库类型
 	'DB_HOST' => 'localhost', //数据库主机
-	'DB_NAME' => '', //数据库名称
-	'DB_USER' => '', //数据库用户名
-	'DB_PWD' => '', //数据库密码
-	'DB_PORT' => '', //数据库端口
-	'DB_PREFIX' => '', //数据库前缀
+	'DB_NAME' => 'yhq_blog', //数据库名称
+	'DB_USER' => 'root', //数据库用户名
+	'DB_PWD' => 'root', //数据库密码
+	'DB_PORT' => '3306', //数据库端口
+	'DB_PREFIX' => 'yhq_', //数据库前缀
 	'DB_CHARSET'=> 'utf8', // 字符集
 	'DB_DEBUG'  => '', // 数据库调试模式 开启后可以记录SQL日志
 	'DB_BIND_PARAM'    =>    true,
